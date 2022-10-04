b4e34b1672c19dfe65406ce2088086a763802fd5
Manuel Toro <manueltoro212113@gmail.com>
2022-10-03 23:43:46 -0300

    Cliente Clase x1

---
+++ TurismoReal.Capa_Negocio/Cliente/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurismoReal.Capa_Negocio.Cliente
+{
+    internal class Cliente
+    {
+ 
+    }
+
+
+    private void CrearUsuario()
+    {
+
+    }
+
+
+}
