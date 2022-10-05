1af0a3a696a995c6c73f1217a44d4e94f4174387
LeandroGir <112043796+LeandroGir@users.noreply.github.com>
2022-10-05 17:03:28 -0400

    Procedimiento almacenado de usuarios y funcion de agregar usuarios a la base de datos

--- TurismoReal-Escritorio/Paginas/Usuarios.xaml.cs
+++ TurismoReal-Escritorio/Paginas/Usuarios.xaml.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Data.OracleClient;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -34,11 +35,36 @@ namespace TurismoReal_Escritorio.Paginas
 
         }
 
+ //       OracleConnection cone = new OracleConnection("DATA SOURCE = xe ; PASSWORD = 123 ; USER ID = TURISMOREALWPF");
 
-        private void BtnGuardarusuario_Click(object sender, RoutedEventArgs e)
+        private void btnGuardarUsuario_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow ventanaPrincipal = new MainWindow();
-            ventanaPrincipal.Show();
+            try 
+            { 
+            cone.Open();
+            OracleCommand comando3 = new OracleCommand("insertar", cone);
+            comando3.CommandType = System.Data.CommandType.StoredProcedure;
+            comando3.Parameters.Add("rut", OracleType.VarChar).Value = TxtRUT.Text;
+            comando3.Parameters.Add("nom", OracleType.VarChar).Value = TxtNombre.Text;
+            comando3.Parameters.Add("apell", OracleType.VarChar).Value = TxtApellido.Text;
+            comando3.Parameters.Add("gene", OracleType.VarChar).Value = TxtGenero.Text;
+            comando3.Parameters.Add("eda", OracleType.VarChar).Value = TxtEdad.Text;
+            comando3.Parameters.Add("est", OracleType.VarChar).Value = TxtEstado_civil.Text;
+            comando3.Parameters.Add("mail", OracleType.VarChar).Value = TxtCorreo.Text;
+            comando3.Parameters.Add("comu", OracleType.VarChar).Value = TxtComuna.Text;
+            comando3.Parameters.Add("contra", OracleType.VarChar).Value = TxtContrasena.Text;
+            comando3.ExecuteNonQuery();
+            MessageBox.Show("Persona agregada a la base de datos");
+                cone.Close();
+            }
+            catch
+            {
+                MessageBox.Show("No se agrego la persona a la base de datos");
+                cone.Close();
+
+            }
+            
+
         }
     }
 }
