ce4373972e5a58405aac11fbeb96e73a1c652a94
Manuel Toro <manueltoro212113@gmail.com>
2022-10-24 21:04:47 -0300

    Create Reportes.cs

---
+++ TurismoReal.Capa_Negocio/Reportes/Reportes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurismoReal.Capa_Negocio.Reportes
+{
+    internal class Reportes
+    {
+    }
+}
