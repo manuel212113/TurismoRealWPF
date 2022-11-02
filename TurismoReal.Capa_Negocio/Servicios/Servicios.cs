26ca2a221f9a4772291f53a4ddb0c39a3bdf4f5e
xeverland <83352027+xeverland@users.noreply.github.com>
2022-11-01 23:41:11 -0300

    Cambios_Semi_listos

---
+++ TurismoReal.Capa_Negocio/Servicios/Servicios.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OracleCommand = Oracle.ManagedDataAccess.Client.OracleCommand;
+using OracleConnection = Oracle.ManagedDataAccess.Client.OracleConnection;
+using OracleDataReader = Oracle.ManagedDataAccess.Client.OracleDataReader;
+using OracleParameter = Oracle.ManagedDataAccess.Client.OracleParameter;
+namespace TurismoReal.Capa_Negocio.Servicios
+{
+    internal class Servicios
+    {
+    }
+}
