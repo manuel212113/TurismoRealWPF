0c702130039faedac2283a9899eb1a8faf9eb46d
xeverland <83352027+xeverland@users.noreply.github.com>
2022-11-18 23:16:45 -0300

    Vista_semi

--- TurismoReal-Escritorio/Paginas/Transporte.xaml.cs
+++ TurismoReal-Escritorio/Paginas/Transporte.xaml.cs
@@ -12,17 +12,63 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TurismoReal.Capa_Negocio.Departamento;
+using TurismoReal.Capa_Negocio.Servicios;
+using System.Runtime.Intrinsics.Arm;
+
+
+using OracleCommand = Oracle.ManagedDataAccess.Client.OracleCommand;
+using OracleConnection = Oracle.ManagedDataAccess.Client.OracleConnection;
+using OracleDataReader = Oracle.ManagedDataAccess.Client.OracleDataReader;
+using OracleParameter = Oracle.ManagedDataAccess.Client.OracleParameter;
+using System.Collections.ObjectModel;
+using MaterialDesignThemes.Wpf;
 
 namespace TurismoReal_Escritorio.Paginas
 {
-    /// <summary>
-    /// Lógica de interacción para Transporte.xaml
-    /// </summary>
+
     public partial class Transporte : Page
     {
+        OracleConnection cone = new OracleConnection("Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=XE)));User Id = C##TR; Password=123");
+        TurismoReal.Capa_Negocio.Transporte.Planificar_transporte trn = new TurismoReal.Capa_Negocio.Transporte.Planificar_transporte();
         public Transporte()
         {
             InitializeComponent();
+            ObservableCollection<TurismoReal.Capa_Negocio.Transporte.Planificar_transporte> Lista1 = new ObservableCollection<TurismoReal.Capa_Negocio.Transporte.Planificar_transporte>();
+            ObservableCollection<TurismoReal.Capa_Negocio.Transporte.Planificar_transporte> Lista2 = new ObservableCollection<TurismoReal.Capa_Negocio.Trasnporte.Planificar_transporte>();
+
+            Lista1 = trn.(Lista2);
+            Lista.ItemsSource = Lista1;
+            Servicio_srv.Text = "TRASNPORTE: " + Lista1.Count.ToString();
+
+
+
+            public void btnAgregarServicio_click(object sender, RoutedEventArgs e)
+            {
+                VentanaAgregarServicioextra.IsOpen = true;
+
+
+            }
+
+            public void btnGuardarServicio_Click(object sender, RoutedEventArgs e)
+            {
+                try
+                {
+                    string CONDUCTOR = TxtNOMBRE_SRV.Text;
+                    string AUTO = TxtPRECIO.Text;
+                    string PATENTE = TxtPATENTE.Text;
+
+
+                    srv.AgregarServicioExtra(NOMBRESRV, PRECIO);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("No se agrego el Departamento a la base de datos");
+                    cone.Close();
+
+                    MessageBox.Show(ex.Message);
+                }
+            }
         }
     }
 }
--- TurismoReal.Capa_Negocio/Transporte/Planificar_transporte.cs
+++ TurismoReal.Capa_Negocio/Transporte/Planificar_transporte.cs
@@ -3,10 +3,83 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OracleCommand = Oracle.ManagedDataAccess.Client.OracleCommand;
+using OracleConnection = Oracle.ManagedDataAccess.Client.OracleConnection;
+using OracleDataReader = Oracle.ManagedDataAccess.Client.OracleDataReader;
+using OracleParameter = Oracle.ManagedDataAccess.Client.OracleParameter;
+
+
+using System.Threading.Tasks;
+using System.Drawing;
+using Imgur.API.Authentication.Impl;
+using Imgur.API.Endpoints.Impl;
+using Imgur.API.Models;
+using Microsoft.Win32;
+using System.Windows.Controls;
+using System.Security.Policy;
+using System.Windows;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+using System.Collections.ObjectModel;
+using TurismoReal.Capa_Negocio.Servicios;
 
 namespace TurismoReal.Capa_Negocio.Transporte
 {
-    internal class Planificar_transporte
+    public class Planificar_transporte
     {
+        public string ID_PLANIFICACION { get; set; }
+        public string CONDUCTOR { get; set; }
+        public string AUTO { get; set; }    
+        public string PATENTE { get; set; } 
+        public string ID_SOLICITUD { get; set; }
+    
+
+
+        public Planificar_transporte()
+        {
+            this.Init();
+        }
+
+        public void Init()
+        {
+            CONDUCTOR = string.Empty;
+            AUTO = string.Empty;
+            PATENTE = string.Empty;
+
+        }
+
+        OracleConnection cone = new OracleConnection("Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=XE)));User Id = C##TR; Password=123");
+
+        public string AgregarTransporte(string CONDUCTOR, string AUTO, string PATENTE)
+        {
+
+            try
+            {
+                cone.Open();
+                OracleCommand ComandoAgregar = new OracleCommand("SP_CREAR_TRANSPORTE", cone);
+                ComandoAgregar.CommandType = System.Data.CommandType.StoredProcedure;
+                ComandoAgregar.Parameters.Add("CONDUCTOR", CONDUCTOR);
+                ComandoAgregar.Parameters.Add("AUTO", AUTO);
+                ComandoAgregar.Parameters.Add("PATENTE", PATENTE);
+
+
+                ComandoAgregar.ExecuteNonQuery();
+                MessageBox.Show("Se agrego el servicio de transporte", "Exito", MessageBoxButton.OK);
+                cone.Close();
+                return "Exito";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se agrego el servicio de transporte", "Error", MessageBoxButton.OK);
+                cone.Close();
+
+                MessageBox.Show(ex.Message);
+                return "Error";
+
+            }
+
+        }
+
     }
+
 }
