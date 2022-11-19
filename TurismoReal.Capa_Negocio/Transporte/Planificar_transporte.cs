0996a4e57875eea75ecdeedccf1986252c60e92a
xeverland <83352027+xeverland@users.noreply.github.com>
2022-11-18 23:16:05 -0300

    Vista_Transporte_Semi

---
+++ TurismoReal.Capa_Negocio/Transporte/Planificar_transporte.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurismoReal.Capa_Negocio.Transporte
+{
+    internal class Planificar_transporte
+    {
+    }
+}
