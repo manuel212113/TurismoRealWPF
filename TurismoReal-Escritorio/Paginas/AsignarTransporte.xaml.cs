7daa8431baddbaf431934e127b67b7a71bae08d9
xeverland <83352027+xeverland@users.noreply.github.com>
2022-12-05 22:56:37 -0300

    FORMULARIO_SEMI

---
+++ TurismoReal-Escritorio/Paginas/AsignarTransporte.xaml.cs
@@ -0,0 +1,53 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TurismoReal.Capa_Negocio.Departamento;
+using TurismoReal.Capa_Negocio.Transporte;
+
+namespace TurismoReal_Escritorio.Paginas
+{
+    /// <summary>
+    /// Lógica de interacción para Actualizar_Transporte.xaml
+    /// </summary>
+    public partial class AsignarTransporte : Window
+    {
+        public AsignarTransporte()
+        {
+            InitializeComponent();
+            AgregarDatosFormulario(CONDUCTOR, AUTO, PATENTE);
+        }
+
+        public void AgregarDatosFormulario(string CONDUCTOR, string AUTO, string PATENTE)
+        {
+            TxtCONDUCTOR.Text = CONDUCTOR;
+            TxtAUTO.Text = AUTO;
+            TxtPATENTE.Text = PATENTE;
+
+        }
+
+        private void btnActualizar_Click(object sender, RoutedEventArgs e)
+        {
+
+            string CONDUCTOR = TxtCONDUCTOR.Text;
+            string AUTO = TxtAUTO.Text;
+            string PATENTE = TxtPATENTE.Text;
+
+            Planificar_transporte transporte = new Planificar_transporte();
+            transporte.AgregarTransporte(CONDUCTOR, AUTO, PATENTE);
+
+        }
+
+    }
+}
